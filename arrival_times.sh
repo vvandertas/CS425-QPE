3a97240cbda3e244729720f93d22db0164af7374
Veerle van der Tas <veerle@vdtas.com>
2019-10-18 17:51:00 +0200

    Some more script and result updates

--- arrival_times.sh
+++ arrival_times.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-for f in $1-conf.json-run*
+echo "job arrival_time"
+for f in $1-conf.json-run$2
 do
-	grep --no-group-separator 'Dispatched ' $f |awk '{split($0,a," "); print a[6] " " a[1] " " a[2]}'
+	grep --no-group-separator 'Dispatched ' $f |awk '{split($0,a," "); print a[6] " " a[1] ":" a[2]}'
 done
 
--- service_times.sh
+++ service_times.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-for f in $1-conf.json-run*
+echo "job service_time end_time"
+for f in $1-conf.json-run$2
 do
-	grep --no-group-separator 'LOG-' $f -A1 |grep --no-group-separator -v 'LOG'|awk '{split($0,a,","); print a[2] " " a[6]-a[5]}'
+	grep --no-group-separator 'LOG-' $f -A1 |grep --no-group-separator -v 'LOG'|awk '{split($0,a,","); print a[2] " " a[6]-a[5] " " a[6]}'
 done
-
